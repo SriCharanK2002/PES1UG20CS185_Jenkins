7bb8a8940f5169678e02f8bc7260f4d6b49e353c
Kanumuri Sri Charan <74090239+SriCharanK2002@users.noreply.github.com>
2023-02-16 11:37:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void)
 {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, Sri Charan has successfully built and run using Jenkins\n";
-  cout << "I am from Bangalore\n";
+  cout << "I, Varsha has successfully built and run using Jenkins\n";
+  cout << "PES1UG20CS185";
   return 0;
 }
